d44d11aa1c6932cb0edca32e5cb1b9ac11238e46
Jonathan <jonathan.riffo7@gmail.com>
2023-08-15 02:08:03 -0300

    Generacion de scaffold report y comment

---
+++ db/migrate/20230815050639_create_reports.rb
@@ -0,0 +1,11 @@
+class CreateReports < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reports do |t|
+      t.string :image
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
